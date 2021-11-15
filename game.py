50596f72f3f3f0dbe22ef83f92dbe6b5c5585a93
Jelly_Pudding <samwalters11111@hotmail.com>
2021-11-15 19:57:06 +0000

    add more enemies and add collision detection

--- game.py
+++ game.py
@@ -2,20 +2,26 @@ import pygame
 import sys
 import random
 
-random_x_number = random.randrange(10, 50, 10)
-random_y_number = random.randrange(10, 50, 10)	
-print(random_x_number)
-print(random_y_number)
+random_list_of_y_coords = random.sample(range(0, 500, 20), 5)
 
 pygame.init()
 screen = pygame.display.set_mode((500, 500))
 the_square = pygame.Rect(10, 240, 10, 10)
-enemy_square = pygame.Rect(490, 240, 10, 10)
+enemy_square_1 = pygame.Rect(490, random_list_of_y_coords[0], 10, 10)
+enemy_square_2 = pygame.Rect(490, random_list_of_y_coords[1], 10, 10)
+enemy_square_3 = pygame.Rect(490, random_list_of_y_coords[2], 10, 10)
+enemy_square_4 = pygame.Rect(490, random_list_of_y_coords[3], 10, 10)
+enemy_square_5 = pygame.Rect(490, random_list_of_y_coords[4], 10, 10)
 pygame.display.set_caption("Square Mover")
+clock = pygame.time.Clock()
 
 def displayer():
     pygame.draw.rect(screen, (0, 255, 0), the_square)
-    pygame.draw.rect(screen, (255, 0, 0), enemy_square)
+    pygame.draw.rect(screen, (255, 0, 0), enemy_square_1)
+    pygame.draw.rect(screen, (255, 0, 0), enemy_square_2)
+    pygame.draw.rect(screen, (255, 0, 0), enemy_square_3)
+    pygame.draw.rect(screen, (255, 0, 0), enemy_square_4)
+    pygame.draw.rect(screen, (255, 0, 0), enemy_square_5)
     pygame.display.update()
 
 running = True
@@ -23,14 +29,27 @@ running = True
 count = 0
 
 while running == True:
-    count += 1
+    clock.tick(10)
     displayer()
     screen.fill((0,0,0))
-    if count % 100 == 0:
-        enemy_square = pygame.Rect.move(enemy_square, -5, 0)   
-    if enemy_square == pygame.Rect(0, 240, 10, 10):
-        #red square reaches the other end of the screen, so gets reset
-        enemy_square = pygame.Rect(490, 240, 10, 10)
+    enemy_square_1 = pygame.Rect.move(enemy_square_1, -5, 0)  
+    enemy_square_2 = pygame.Rect.move(enemy_square_2, -5, 0)
+    enemy_square_3 = pygame.Rect.move(enemy_square_3, -5, 0)
+    enemy_square_4 = pygame.Rect.move(enemy_square_4, -5, 0)
+    enemy_square_5 = pygame.Rect.move(enemy_square_5, -5, 0) 
+    # If an enemy square hits our green square, the game ends
+    if pygame.Rect.colliderect(the_square, enemy_square_1) == True or pygame.Rect.colliderect(the_square, enemy_square_2) == True or pygame.Rect.colliderect(the_square, enemy_square_3) == True or pygame.Rect.colliderect(the_square, enemy_square_4) == True or pygame.Rect.colliderect(the_square, enemy_square_5) == True:
+        print("Game Over!")
+        pygame.quit()
+        sys.exit()
+    if enemy_square_1[0] == -20:
+        #red squares reache the other end of the screen, so they all get reset
+        random_list_of_y_coords = random.sample(range(0, 500, 20), 5)
+        enemy_square_1 = pygame.Rect(490, random_list_of_y_coords[0], 10, 10)
+        enemy_square_2 = pygame.Rect(490, random_list_of_y_coords[1], 10, 10)
+        enemy_square_3 = pygame.Rect(490, random_list_of_y_coords[2], 10, 10)
+        enemy_square_4 = pygame.Rect(490, random_list_of_y_coords[3], 10, 10)
+        enemy_square_5 = pygame.Rect(490, random_list_of_y_coords[4], 10, 10)
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_q:
